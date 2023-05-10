b8d22e28108356a0f0717665ba414b5287e96794
NV <nadity6@gmail.com>
2023-05-10 14:25:10 +0700

    up app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+# hitung luas segitiga sederhana upload di gutbhub
+def luas_segitiga(): 
+    a = int(input("Masukkan Alas Segitiga : "))
+    t = int(input("Masukkan Tinggi Segitiga : "))
+    luas = a * t / 2
+    print ("luas Segitiga Adalah: ", luas)
+luas_segitiga()
